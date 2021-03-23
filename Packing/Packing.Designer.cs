1c4aefce0481c49809fcfed9a914c486aac69ce1
JONATHAN AGUILAR <jjaguilar@unitec.edu>
2021-03-23 15:32:32 -0600

    Inicio diseño de FormPacking

---
+++ Packing/Packing.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Packing
+{
+    partial class Packing
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUpcTag = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUpcTag);
+            this.groupBox1.Location = new System.Drawing.Point(30, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(613, 167);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtUpcTag
+            // 
+            this.txtUpcTag.Location = new System.Drawing.Point(59, 51);
+            this.txtUpcTag.Name = "txtUpcTag";
+            this.txtUpcTag.Size = new System.Drawing.Size(479, 26);
+            this.txtUpcTag.TabIndex = 1;
+            // 
+            // Packing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1581, 928);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Packing";
+            this.Text = "Packing";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUpcTag;
+    }
+}
+
--- Packing/Packing.cs
+++ Packing/Packing.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Packing
 {
-    public partial class Form1 : Form
+    public partial class Packing : Form
     {
-        public Form1()
+        public Packing()
         {
             InitializeComponent();
         }
