9a7884c2ffe52fc31f093e04554a6771a6ffce8c
JONATHAN AGUILAR <jjaguilar@unitec.edu>
2021-03-24 09:23:10 -0600

    Revert "connection string config"
    This reverts commit 9ce4574e8e36fd57e0f04ab9e66cec32c368c302.

--- Packing/Packing.cs
+++ Packing/Packing.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,8 +12,6 @@ namespace Packing
 {
     public partial class Packing : Form
     {
-        static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
-        public string constring = "";
         public Packing()
         {
             InitializeComponent();
